b44c975d560ea00c7904e3a4769563e247bcca85
Avinash <dareddy2005@gmail.com>
2026-08-21 00:38:20 +0530

    Front end URL is added

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://mcp-orchestrator-chi.vercel.app/"
+        "https://mcp-orchestrator-bay.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
